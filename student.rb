628f76f8540fab2623cf5b3cf9935813040adf26
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-18 09:50:51 +0430

    Add classroom add student call in Student class

--- student.rb
+++ student.rb
@@ -4,6 +4,7 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parents_permission = 'true')
     super(age, name, parents_permission)
     @classroom = classroom
+    @classroom.add_student(self)
   end
 
   def play_hooky
