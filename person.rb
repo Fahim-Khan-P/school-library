4bbefcee0f3230dfeb05512ea0dbb4cecb09ca57
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-18 09:48:56 +0430

    Update person class and add rental related data

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ require_relative 'trimmer_decorator'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -12,6 +12,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -29,6 +30,11 @@ class Person < Nameable
   def correct_name
     @name
   end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
 end
 
 person = Person.new(22, 'maximilianus')
