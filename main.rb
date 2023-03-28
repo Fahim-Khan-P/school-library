719b238239a8b1334c2cc1dce0d7fa589b9dc0a4
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-28 13:28:56 +0430

    Add required changes!(Input/output should be in main.rb)

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 require './app'
 require './option'
-require './show_menu'
 
 class Main
   def initialize
@@ -10,7 +9,14 @@ class Main
   def main
     puts 'welcome to school library App!'
     loop do
-      show_menu
+      puts "\nPlease choose an option by entering a number:
+  1 - List all books
+  2 - List all people
+  3 - Create a person
+  4 - Create a book
+  5 - Create a rental
+  6 - List all rentals for a given person id
+  7 - Exit"
       user_input = gets.chomp
       process_options(user_input)
     end
