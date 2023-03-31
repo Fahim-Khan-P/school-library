065b6e7948b8405b773731cc2d8f58b2473bdc93
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-31 11:29:59 +0430

    Add test to person!

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -13,6 +13,10 @@ describe Book do
       expect(@book).to be_an_instance_of Book
     end
 
+    it 'Should create an instance of the book' do
+      expect(@person).to be_an_instance_of Person
+    end
+
     it 'Should return title and author' do
       expect @book.title.should eq 'newTitle'
       expect @book.author.should eq 'newAuthor'
