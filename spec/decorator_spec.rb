fdd3f0aa220e2a36e6b3d8fa1ee3df6d02637e42
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-31 22:50:32 +0430

    Create separate file for trimmer test!

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,28 +1,7 @@
-require_relative '../trimmer_decorator'
+
 require_relative '../capitalize_decorator'
 
-RSpec.describe TrimmerDecorator do
-  describe '#correct_name' do
-    let(:nameable) { double('nameable', correct_name: 'Khan') }
-    let(:decorator) { TrimmerDecorator.new(nameable) }
 
-    it 'returns the full name when it is shorter than the maximum length' do
-      expect(decorator.correct_name).to eq('Khan')
-    end
-
-    it 'returns a truncated name when it is longer than the maximum length' do
-      nameable = double('nameable', correct_name: 'Fahim Khan Wardak')
-      decorator = TrimmerDecorator.new(nameable)
-      expect(decorator.correct_name).to eq('Fahim Khan')
-    end
-
-    it 'returns an empty string when the decorated object returns an empty string' do
-      nameable = double('nameable', correct_name: '')
-      decorator = TrimmerDecorator.new(nameable)
-      expect(decorator.correct_name).to eq('')
-    end
-  end
-end
 
 describe CapitalizeDecorator do
   describe '#correct_name' do
