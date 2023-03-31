9d97a6daaa5569be46722a7c2bc7add4998bfad7
tonnymuchui <tonykanyingah@gmail.com>
2023-03-31 17:33:24 +0300

    fix Teacher test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -3,7 +3,7 @@ require_relative '../teacher'
 describe Teacher do
   describe '#initialize' do
     it 'creates a new Teacher object' do
-      teacher = Teacher.new(10, 'Math', 'Tonny', true)
+      teacher = Teacher.new(10, 'Math', 'Tonny')
       expect(teacher).to be_an_instance_of Teacher
     end
   end
