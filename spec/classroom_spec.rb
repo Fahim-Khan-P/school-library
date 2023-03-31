e146cdcd42ba3abca8c1b4ee412b1a184f1f9be6
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-31 11:33:33 +0430

    Add classroom testing!

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../classroom'
+
+describe Classroom do
+  describe '#initialize' do
+    it 'creates a new Classroom object' do
+      classroom = Classroom.new('label')
+      expect(classroom).to be_an_instance_of Classroom
+    end
+  end
+end
